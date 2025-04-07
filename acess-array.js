02383acabd4b7eb183477081c7f4345039aeff9a
KAVYA SRI A S <146112532+Kavyasri-AS@users.noreply.github.com>
2025-04-07 15:18:12 +0530

    acess-array.js

---
+++ acess-array.js
@@ -0,0 +1,9 @@
+//access elements from array
+const a = [1, 2, 3, 4]
+console.log(a[1]) //second
+const [b, c, d, e] = a //direct acess
+console.log(e)
+    // array destructing;
+const food = ["idlly", "dosai", "vada", "pori"]
+const [food1, food2, food3, food4] = food
+console.log(food1)
