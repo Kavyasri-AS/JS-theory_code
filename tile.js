65b4550dcd962b0b9df4f4742ccafef9c385b48d
KAVYA SRI A S <146112532+Kavyasri-AS@users.noreply.github.com>
2025-04-07 15:17:28 +0530

    tile.js

---
+++ tile.js
@@ -0,0 +1,9 @@
+let name = "Kavs";
+let college = "KRCE";
+let cgpa = 9.3;
+let course = "ECE";
+
+console.log(`My name is ${name}.`);
+console.log(`I studied at ${college}.`);
+console.log(`My CGPA is ${cgpa}.`);
+console.log(`I belong to the ${course} department.`);
